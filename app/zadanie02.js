e40007d75ab0f723612d6f2e2eb8d30f17414137
Hubert Kozłowski <hubert890@poczta.onet.pl>
2018-09-21 14:10:01 +0200

    day5

--- app/zadanie02.js
+++ app/zadanie02.js
@@ -1,1 +1,30 @@
-//Twój kod
+const express = require('express');
+const app = express();
+
+const names = [];
+
+app.get('/name/set/:imie', (req, res) => {
+		const name = req.params.imie;
+		names.push(name);
+    res.send(`<h3> Podane imię: ${name} <h3>`);
+});
+
+app.get('/name/show', (req, res) => {
+		if (names.indexOf(names[names.length - 1]) < 0) {
+				res.send(`<h3> Nie podano żadnego imienia </h3>`);
+		} else {
+				res.send(`<h3> Ostatnie imię, które podano: ${names[names.length - 1]} </h3>`);
+		}
+});
+
+app.get('/name/check', (req, res) => {
+		if (names[names.length] < 1) {
+				res.send(`<h3> Zbiór jest pusty </h3>`);
+		} else {
+				res.send(`<h3> Zbiór nie jest pusty </h3>`);
+		}
+});
+
+app.listen(3000, () => {
+    console.log('Serwer uruchomiony na porcie 3000');
+});
--- app/zadanieDnia1.js
+++ app/zadanieDnia1.js
@@ -1,1 +1,24 @@
-//Twój kod
+const express = require('express');
+const app = express();
+
+const counter = {
+		yes: 0,
+		no: 0
+}
+
+app.use(express.static('./public/zadanieDnia/'));
+
+app.get('/vote/:vote', (req, res) => {
+		const voting = req.params.vote;
+		counter[voting] += 1;
+    res.send(`<h3> Dziękujemy za głos <h3>`);
+});
+
+
+app.get('/votes/check', (req, res) => {
+    res.send(`Zagłosowano na tak: ${counter.yes} <br> Zagłosowano na nie: ${counter.no}`);
+});
+
+app.listen(3000, () => {
+    console.log('Serwer uruchomiony na porcie 3000');
+});
