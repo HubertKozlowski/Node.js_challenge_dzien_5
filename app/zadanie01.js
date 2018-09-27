8ed7b4a78018f1424041a255f82787c80cb186dd
Hubert Kozłowski <hubert890@poczta.onet.pl>
2018-09-27 14:53:23 +0200

    fixed indentation

--- app/zadanie01.js
+++ app/zadanie01.js
@@ -2,12 +2,12 @@ const express = require('express');
 const app = express();
 
 app.get('/:number1/:number2', (req, res) => {
-		const number1 = parseInt(req.params.number1);
-		const number2 =  parseInt(req.params.number2);
-		const sum = number1 + number2;
-    res.send(`${number1} + ${number2} = ${sum}`);
+  const number1 = parseInt(req.params.number1);
+  const number2 =  parseInt(req.params.number2);
+  const sum = number1 + number2;
+  res.send(`${number1} + ${number2} = ${sum}`);
 });
 
 app.listen(3000, () => {
-    console.log('Serwer uruchomiony na porcie 3000');
+  console.log('Serwer uruchomiony na porcie 3000');
 });
--- app/zadanieDnia1.js
+++ app/zadanieDnia1.js
@@ -2,23 +2,23 @@ const express = require('express');
 const app = express();
 
 const counter = {
-		yes: 0,
-		no: 0
+  yes: 0,
+  no: 0
 }
 
 app.use(express.static('./public/zadanieDnia/'));
 
 app.get('/vote/:vote', (req, res) => {
-		const voting = req.params.vote;
-		counter[voting] += 1;
-    res.send(`<h3> Dziękujemy za głos <h3>`);
+  const voting = req.params.vote;
+  counter[voting] += 1;
+  res.send(`<h3> Dziękujemy za głos <h3>`);
 });
 
 
 app.get('/votes/check', (req, res) => {
-    res.send(`Zagłosowano na tak: ${counter.yes} <br> Zagłosowano na nie: ${counter.no}`);
+  res.send(`Zagłosowano na tak: ${counter.yes} <br> Zagłosowano na nie: ${counter.no}`);
 });
 
 app.listen(3000, () => {
-    console.log('Serwer uruchomiony na porcie 3000');
+  console.log('Serwer uruchomiony na porcie 3000');
 });
